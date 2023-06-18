9a7706e962a71dd86088488ef1382d7b59706165
AsafEphraim <117855177+AsafEphraim@users.noreply.github.com>
2023-06-18 23:02:10 +0300

    Add files via upload
    updated code according to system design + state machine

--- ESP32/userSetupMgr.cpp
+++ ESP32/userSetupMgr.cpp
@@ -1,30 +1,34 @@
 #include "userSetupMgr.h"
 
+#include <WiFi.h>
+#include <WebServer.h>
+
 //true: creates an access point, false: connects to an existing wifi
 const bool AccessPointMode = true;
 //wifi credentials (enter yours if you arne not using the AccessPointMode)
-const char *ssid = "VGA Smart Signage";
+const char *ssid = "VGA";
 const char *password = "";
 
 const char* ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 10800;
 const int daylightOffset_sec = 0;
-
 WebServer server(80); // create a web server on port 80
 
-// tm& CUserSetupMgr::getTime()
-// {
+ struct tm timeinfo;
 
-// }
-
-void printLocalTime()
+tm& CUserSetupMgr::getNTPValue()
 {
-  struct tm timeinfo;
+  return timeinfo;
+}
+
+
+void saveLocalTime()
+{
+ 
   if (!getLocalTime(&timeinfo)) {
     Serial.println("Falid to obtain time");
     return;
   }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
 }
 
 void handleConnect()
@@ -37,7 +41,7 @@ void handleConnect()
   }
   Serial.println("CONNECTED");
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
+  saveLocalTime();
   Serial.println(server.arg("ssid").c_str());
   server.send(200, "text/html", "<h1>Connected to WiFi network</h1>"); // display success message
 
@@ -50,7 +54,7 @@ void handleRoot()
 
 void CUserSetupMgr::doSetup()
 {
-        //Handle the WiFi AP or STA mode and display results on the screen
+    //Handle the WiFi AP or STA mode and display results on the screen
     if (AccessPointMode)
     {
         Serial.println("Creating access point...");
@@ -73,17 +77,29 @@ void CUserSetupMgr::doSetup()
     server.begin(); // start web server
 }
 
-void CUserSetupMgr::doLoopLogic()
+
+NUserSetup::EWIFI_STATUS CUserSetupMgr::doLoopLogic()
 {
+  int time_count = 0;
+  while(true)
+  {
     //TODO: make this functionality only if system state is ok
     server.handleClient(); // handle incoming client requests
     if (WiFi.status() != WL_CONNECTED) { // if not connected to WiFi network
+        if(time_count > 20)
+        {
+          //After 20 seconds it coulnd't connect to Wifi, needs to start again.
+          return NUserSetup::E_WRONG_WIFI_DETAILS;
+        }
         Serial.println("Connecting to WiFi network..."); // display connection message
         delay(1000); // wait for connection
+        time_count ++;
     }
     else {
         Serial.println("CONNECTED");
         delay(1000); // wait for connection
-        printLocalTime();
+        saveLocalTime();
+        return NUserSetup::E_CONNECTED;
     }
+  }
 }
--- ESP32/userSetupMgr.h
+++ ESP32/userSetupMgr.h
@@ -1,19 +1,25 @@
 #ifndef _USERSETUPMGR
 #define _USERSETUPMGR
 
-#include <WiFi.h>
-#include <WebServer.h>
 #include "time.h"
+
+namespace NUserSetup
+{
+    enum EWIFI_STATUS
+    {
+    E_CONNECTED,
+    E_WRONG_WIFI_DETAILS,
+    };
+};
+
 
 class CUserSetupMgr
 {
 public:
     CUserSetupMgr() = default;
     void doSetup();
-    void doLoopLogic();
-    // tm& getTime();
-    //getManaulData();
-
+    NUserSetup::EWIFI_STATUS doLoopLogic();
+    tm& getNTPValue();
 };
 
 #endif
