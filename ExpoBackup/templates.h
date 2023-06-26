7296165e3cbbc68d703d38ac4ff8d01e79b26986
AsafEphraim <117855177+AsafEphraim@users.noreply.github.com>
2023-06-26 22:23:35 +0300

    Add files via upload
    full display version for expo

---
+++ ExpoBackup/templates.h
@@ -0,0 +1,19 @@
+#ifndef _TEMPLATES
+#define _TEMPLATES
+
+#include <Arduino.h>
+
+struct template1Data {
+    String backgroundColor;
+    String mainHeadline;
+    String task1;
+    String task2;
+    String task3;
+    String task4;
+    String taskBackgroundColor;
+};
+
+struct template2Data {
+};
+
+#endif
